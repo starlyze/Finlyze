080f60c22592441b66f7ffd844da23d1d1ce65bd
Darren Chu <darren.cw.chu@gmail.com>
2024-12-06 02:46:44 -0500

    initial commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import {HomeComponent} from "./home/home.component";
+import {SigninComponent} from "./signin/signin.component";
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'signin', component: SigninComponent },
+];
