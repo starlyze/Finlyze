e2578bb4837e0663e5479b52768fff0202356328
Darren Chu <darren.cw.chu@gmail.com>
2024-12-07 16:45:44 -0500

    signup page

--- src/app/signup/signup.component.ts
+++ src/app/signup/signup.component.ts
@@ -1,9 +1,14 @@
 import { Component } from '@angular/core';
+import {CanvasBackgroundComponent} from "../components/canvas-background/canvas-background.component";
+import {RouterLink} from "@angular/router";
 
 @Component({
   selector: 'app-signup',
   standalone: true,
-  imports: [],
+  imports: [
+    CanvasBackgroundComponent,
+    RouterLink
+  ],
   templateUrl: './signup.component.html',
   styleUrl: './signup.component.scss'
 })
