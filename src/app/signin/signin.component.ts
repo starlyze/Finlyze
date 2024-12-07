daf805f7f84d729fdd3f00401661ba3fc8bfafb1
Darren Chu <darren.cw.chu@gmail.com>
2024-12-07 15:31:07 -0500

    updated sign in page

--- src/app/signin/signin.component.ts
+++ src/app/signin/signin.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import {CanvasBackgroundComponent} from "../components/canvas-background/canvas-background.component";
+import {RouterLink} from "@angular/router";
 
 @Component({
   selector: 'app-signin',
   standalone: true,
   imports: [
-    CanvasBackgroundComponent
+    CanvasBackgroundComponent,
+    RouterLink
   ],
   templateUrl: './signin.component.html',
   styleUrl: './signin.component.scss'
